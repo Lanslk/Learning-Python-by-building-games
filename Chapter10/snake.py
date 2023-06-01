28c92a1802678ce70b4ad003c2d4d89df848e633
Lanslk <wolf810210@hotmail.com>
2023-06-01 11:00:37 +0800

    modify snake.py

--- Chapter10/snake.py
+++ Chapter10/snake.py
@@ -17,29 +17,56 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+# Pen
+pen = Turtle()
+pen.speed(0)
+pen.color('black')
+pen.penup()
+pen.goto(0, 150)
+pen.write("Score : 0", align='center', font=('Courier', 24, 'bold'))
+pen.hideturtle()
+
+
 def change(x, y):
     "Change snake direction."
-    aim.x = x
-    aim.y = y
+    if aim + vector(x, y) == vector(0, 0):
+        return
+    else:
+        aim.x = x
+        aim.y = y
+
 
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
 
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
 
+    if head.x < -200:
+        head.x = 190
+    elif head.x > 190:
+        head.x = -200
+    elif head.y < -200:
+        head.y = 190
+    elif head.y > 190:
+        head.y = -200
+    update()
+
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
+        # print('Snake:', len(snake))
+        pen.clear()
+        pen.write("Score : {}".format(len(snake) - 1), align='center', font=('Courier', 24, 'bold'))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
@@ -52,7 +79,8 @@ def move():
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    ontimer(move, 100)  # snake speed
+
 
 setup(420, 420, 370, 0)
 hideturtle()
